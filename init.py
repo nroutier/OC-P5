661fdb5e198f73c254510aad0e4bb9fc5b472882
unknown <nroutier@gmail.com>
2019-02-20 00:39:52 +0100

    correcting init

--- init.py
+++ init.py
@@ -8,8 +8,8 @@ NUMBER_OF_CATEGORIES = 20
 NUMBER_OF_PRODUCTS = 30
 
 # MySQL connection information
-USER = "svc_oc_p5"
-PASSWORD = "passw0rd"
-DATABASE = "OC_P5"
-HOST = "172.29.29.20"
-PORT = 3306
+USER = ""
+PASSWORD = ""
+DATABASE = ""
+HOST = ""
+PORT = 0
